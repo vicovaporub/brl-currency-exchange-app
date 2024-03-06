114da264bfc5a86d74d1d5122356cb0cab1384cc
Victor Castro <victor.castro34@gmail.com>
2024-03-06 14:53:31 -0300

    feat: moved convertion function to convertion component

--- src/components/BrlValue.tsx
+++ src/components/BrlValue.tsx
@@ -1,33 +1,4 @@
-"use client";
-
-import { fetchCurrencies } from "@/api/fetches";
-
-export const BrlValue = ({ setCurrencyValue }: any) => {
-  const convertToggle = () => {
-    let inputValue = document.getElementById("brlValue") as HTMLInputElement;
-    if (
-      inputValue.value === "" ||
-      inputValue.value === null ||
-      isNaN(parseInt(inputValue.value)) ||
-      parseInt(inputValue.value) < 0
-    ) {
-      alert("Please input a valid value");
-      inputValue.value = "";
-      return;
-    } else {
-      fetch("mockApi.json")
-        .then((response) => response.json())
-        .then((data) => {
-          const valueToConvert = parseFloat(inputValue.value);
-          const brlToUsd = 1 / data.data.BRL;
-
-          const conversion = valueToConvert * brlToUsd;
-          console.log(conversion.toFixed(2).toString());
-          setCurrencyValue(conversion.toFixed(2));
-        });
-    }
-  };
-
+export const BrlValue = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center m-5">
@@ -43,13 +14,6 @@ export const BrlValue = ({ setCurrencyValue }: any) => {
           />
         </div>
       </div>
-      <button
-        onClick={convertToggle}
-        className="h-[3.2rem] p-3 flex justify-center border rounded-lg
-         border-gray-400 bg-blue-900 hover:bg-blue-800 text-white"
-      >
-        Convert
-      </button>
     </>
   );
 };
