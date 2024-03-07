84eedf5d04e6721a6502d36293ea8caa0b49167d
Victor Castro <victor.castro34@gmail.com>
2024-03-07 11:55:03 -0300

    feat: adjusted the official fetch and added comment to fetch mock

--- src/api/fetches.ts
+++ src/api/fetches.ts
@@ -7,8 +7,12 @@ export const fetchCurrencies = async () => {
   );
   const data = await response.json();
 
-  return data;
+  return new CurrencyClass(data.data);
 };
+
+//This next function is used to fetch the mock data from the mockApi.json file
+//it was used on tests and on the development of the app
+//it is not used on the production version of the app
 
 export const fetchMockCurrencies = async () => {
   const response = await fetch("mockApi.json");
