d3216daed63dbbef25f2a3816740dc663b0138ec
Victor Castro <victor.castro34@gmail.com>
2024-03-07 12:01:12 -0300

    feat: added key

--- src/api/fetches.ts
+++ src/api/fetches.ts
@@ -1,7 +1,7 @@
 import { CurrencyClass } from "@/shared/CurrencyClass";
 
 export const fetchCurrencies = async () => {
-  const apiKey = process.env.NEXT_PUBLIC_CURRENCY_API_KEY;
+  const apiKey = process.env.CURRENCY_API_KEY;
   const response = await fetch(
     `https://api.freecurrencyapi.com/v1/latest?apikey=${apiKey}`
   );
